3be62a09fca5192dd75fc981af01d7a127bae2fe
guna-design <gunasekar20101999.com>
2024-02-18 11:48:57 +0530

    first commit

---
+++ Note/src/Context/Notedata.jsx
@@ -0,0 +1,21 @@
+// import React, { useState } from "react";
+
+// export const NoteData = React.createContext();
+
+// function NotesContext({ children }) {
+//   const [data, setData] = useState([
+//     {
+//       title: "Sample Note",
+//       note: "Create a Note By Adding Title & Note in Required Feild given & Click Enter To Edit click edit Symbol on th top right of the Note Card edit the Note and again click enter to Save the Changes",
+      
+//     },
+//   ]);
+//   const [editIndex, setEditIndex] = useState(null);
+//   return (
+//     <NoteData.Provider value={{ data, setData, editIndex, setEditIndex }}>
+//       {children}
+//     </NoteData.Provider>
+//   );
+// }
+
+// export default NotesContext;
